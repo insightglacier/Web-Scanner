704865f3d33bf502d87566c5aa23e5922a428967
Jeffrey Chan <jeffschan97@gmail.com>
2016-08-01 15:10:22 -0700

    Implemented ctrl-f function, but has bugs

--- script.py
+++ script.py
@@ -4,30 +4,40 @@
 
 import urllib2
 
-def wordOccurrence(word, page):
-    # returns the number of times a word occurs within a page
-    print 'running wordOccurrence'
-    return 0
+def htmlCrawler(word, html):
+    # returns the number of times given word appears in given html
+    # case sensitive
+    counter = htmlIndex = wordIndex = 0
+    while htmlIndex < len(html): # iterate through all the chars in html
+        while (wordIndex < len(word) and htmlIndex < len(html) and html[htmlIndex] == word[wordIndex]):
+            wordIndex += 1
+            if wordIndex == len(word):
+                counter += 1
+                wordIndex = 0
+            htmlIndex += 1
+        htmlIndex += 1
+        wordIndex = 0
+    return counter
 
 print 'Welcome to Launch Locator!'
 print 'This program will now begin searching for launch information that might help you!\n'
 
-# Reads in list of URLs fromm ./config/WebsiteList.txt
+# Reads URLs from ./config/WebsiteList.txt and stores in websiteList
 websiteListFile = open("./config/WebsiteList.txt", "r")
 websiteList = websiteListFile.read().split('\n')
 websiteListFile.close()
 
-# Reads in list of keywords from ./config/Keywords.txt
+# Reads keywords from ./config/Keywords.txt and stores in keywordList
 keywordFile = open('./config/Keywords.txt', 'r')
 keywordList = keywordFile.read().split('\n')
 keywordFile.close()
 
 # Make program be able to visit website on the website list
 for url in websiteList:
-    page = urllib2.urlopen(url).read()
-    for word in keywordList:
-        wordFrequency = wordOccurrence(keywordList[0], page)
-        print 'The word', word, 'appeared', wordFrequency, 'times.'
+    page = urllib2.urlopen(url).read().lower() # stores lowercase page HTML into variable
+    for keyword in keywordList:
+        wordAppearance = htmlCrawler(keyword.lower(), page)
+        print 'The word', keyword, 'appeared', wordAppearance, 'times.'
 
 # Make program count the number of times a keyword appears
 
