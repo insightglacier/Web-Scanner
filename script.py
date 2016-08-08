53d53c374e0635a7c40a55606cd66c63e427432b
Jeffrey Chan <jeffschan97@gmail.com>
2016-08-08 14:49:47 -0700

    Added ability to crawl through embedded links

--- script.py
+++ script.py
@@ -4,6 +4,8 @@
 
 import urllib2
 import smtplib
+from bs4 import BeautifulSoup
+from sets import Set
 
 def htmlCrawler(word, html):
     # returns the number of times given word appears in given html
@@ -33,9 +35,23 @@ keywordFile = open('./config/Keywords.txt', 'r')
 keywordList = keywordFile.read().split('\n')
 keywordFile.close()
 
+urlSet = Set([]) # create an empty set to hold other links
+
 # Pull HTML from listed websites and look for keywords
 for url in websiteList:
     page = urllib2.urlopen(url).read().lower() # stores lowercase page HTML into variable
+    soup = BeautifulSoup(page, 'html.parser') # pass html into BeautifulSoup constructor
+    for link in soup.find_all('a'):         # find external links and add them to urlSet
+        urlSet.add(link.get('href'))
+    print 'Looking for keywords within', url
+    for keyword in keywordList:
+        wordAppearance = htmlCrawler(keyword.lower(), page)
+        print 'The word', keyword, 'appeared', wordAppearance, 'times.'
+    print '\n'
+
+# Pull HTML from external websites and look for keywords
+for url in urlSet:
+    page = urllib2.urlopen(url).read().lower()
     print 'Looking for keywords within', url
     for keyword in keywordList:
         wordAppearance = htmlCrawler(keyword.lower(), page)
